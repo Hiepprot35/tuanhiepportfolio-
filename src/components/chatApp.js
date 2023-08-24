b885b1fe3c55a722a808186fc86fed95c46bf9e7
Hiepprot3 <tansou57@gmail.com>
2023-08-25 01:45:38 +0700

    oke

--- src/components/chatApp.js
+++ src/components/chatApp.js
@@ -1,21 +1,63 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
+import useAuth from '../hook/useAuth';
+import BlobtoBase64 from '../function/BlobtoBase64';
 
 const ChatApp = (prop) => {
+  const { auth } = useAuth()
+  const [isLoading, setIsLoading] = useState(true)
+  const [user, setUser] = useState()
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
-  const socket = io.connect('https://tuanhiepprot3api.onrender.com/'); // Replace with your server URL
+  const socket = io.connect(process.env.REACT_APP_DB_HOST); // Replace with your server URL
+  useEffect(() => {
+    let isAlivew = true;
 
+    const studentInfo = async () => {
+      const URL = `${process.env.REACT_APP_DB_HOST}/api/getStudentbyID`;
+      try {
+        const studentApi = await fetch(URL, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+
+          },
+          body:
+            JSON.stringify(
+              {
+                "username": auth.username
+              }
+            )
+        });
+        if (isAlivew) {
+
+          const student = await studentApi.json();
+
+          setUser(student)
+          console.log(student)
+          setIsLoading(false)
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    studentInfo();
+    return () => {
+      isAlivew = false
+    }
+  }, []);
   useEffect(() => {
     socket.on('recevie_messsage', (message) => {
-      setMessages([...messages,message]);
+      setMessages([...messages, message]);
     });
   }, [messages]);
 
   const sendMessage = () => {
-    socket.emit('SendMessage',{
-      UserID:prop.user.userID,
-      Message:inputMessage});
+    socket.emit('SendMessage', {
+      UserID: prop.user.userID,
+      Message: inputMessage,
+      img: user.img
+    });
     setInputMessage('');
   };
 
@@ -23,7 +65,14 @@ const ChatApp = (prop) => {
     <div className='ChatApp'>
       <div>
         {messages.map((message, index) => (
-          <div key={index}>{message.UserID}: {message.Message}</div>
+          <div key={index}>
+            
+
+           
+              {message && <img src={`${BlobtoBase64(message.img)}`} className='avatarImage' alt='User Avatar' />}
+              :{message.Message}
+          
+          </div>
         ))}
       </div>
       <input
