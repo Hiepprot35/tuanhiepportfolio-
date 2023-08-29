7d86abcbc75b774162a9369d82829dead7dc89f4
Hiepprot3 <tansou57@gmail.com>
2023-08-29 17:35:04 +0700

    oke

--- src/components/chatapp/chatApp.js
+++ src/components/chatapp/chatApp.js
@@ -263,7 +263,7 @@ const ChatApp = (prop) => {
             <div className='Narbar_ChatApp'>
               <input placeholder="Search for friends" className="chatMenuInput" />
 
-              {conversations.length!==0  && conversations.map((c, index) => (
+              {conversations && Array.isArray(conversations) &&conversations.length!==0  && conversations.map((c, index) => (
                 <div onClick={() => setCurrentChat(c)} key={index} className='converrsation_chat' style={currentChat === c ? { backgroundColor: "rgb(245, 243, 243)" } : {}} >
                   <Conversation conversation={c} currentUser={auth.userID} Arrivalmess={arrivalMessage} mess={messages.length} Online={onlineUser} />
                 </div>
